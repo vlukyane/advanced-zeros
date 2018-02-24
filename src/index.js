08986958592ac5cc90926c1ac274f987162f3a44
vlukyaneRubyLabs <vlukyane@gmail.com>
2018-02-24 10:53:07 +0300

    adv. zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+module.exports = function getZerosCount(n, b) {
+    var j = b, ans = n, p = 0, res = 0, k = 0;
+    for (var i = 2; i <= b; i++){
+        if (j % i === 0){
+            p = 0;
+            while (j % i === 0){
+                p++;
+                j /=i;
+            }
+            res = 0, k = n;
+            while (k / i > 0){
+                res += Math.floor(k / i);
+                k = Math.floor(k / i);
+            }
+            ans = Math.min(ans, Math.floor(res / p));
+        }
+    }
+    return ans;
+};
+
+//getZerosCount(60559525, 147);
